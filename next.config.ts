aab20a5ee65822255313aa28ce733d488eb1807c
Alejo Cortiñaz <masalejo8@gmail.com>
2026-01-24 17:23:17 -0300

    Add CSP headers to fix NextAuth login issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data:; connect-src 'self' https:; frame-src 'self';"
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
