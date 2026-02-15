b68b8c62b4652d7f556ce00dea305828dcbe532a
Alejo Cortiñaz <masalejo8@gmail.com>
2026-02-15 13:14:27 -0300

    refactor: make schedules redesign editor canonical

--- web/app/dashboard/schedules/[scheduleId]/page.tsx
+++ web/app/dashboard/schedules/[scheduleId]/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { use } from "react";
-import ScheduleEditor from "@/components/schedules/schedule-editor";
 import ScheduleGridEditor from "@/components/schedules-redesign/schedule-grid-editor";
-
-const schedulesRedesignEnabled = process.env.NEXT_PUBLIC_SCHEDULES_REDESIGN_ENABLED === "true";
 
 export default function ScheduleEditorPage({ params }: { params: Promise<{ scheduleId: string }> }) {
     const { scheduleId } = use(params);
@@ -16,11 +13,7 @@ export default function ScheduleEditorPage({ params }: { params: Promise<{ sched
             </div>
 
             <div className="flex-1 bg-white shadow rounded-lg overflow-hidden flex flex-col">
-                {schedulesRedesignEnabled ? (
-                    <ScheduleGridEditor scheduleId={scheduleId} />
-                ) : (
-                    <ScheduleEditor scheduleId={scheduleId} />
-                )}
+                <ScheduleGridEditor scheduleId={scheduleId} />
             </div>
         </div>
     );
